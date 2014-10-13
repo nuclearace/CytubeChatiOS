95a49c509b012bbcbbd1f5519018574d438f1f2c
Erik <nuclear.ace@gmail.com>
2014-10-13 12:12:07 -0400

    Initial Commit

---
+++ CytubeChat1.0/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  CytubeChat1.0
+//
+//  Created by Erik Little on 10/13/14.
+//  Copyright (c) 2014 Tracy Cage Industries. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ CytubeChat1.0Tests/CytubeChat1_0Tests.swift
@@ -0,0 +1,36 @@
+//
+//  CytubeChat1_0Tests.swift
+//  CytubeChat1.0Tests
+//
+//  Created by Erik Little on 10/13/14.
+//  Copyright (c) 2014 Tracy Cage Industries. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class CytubeChat1_0Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
