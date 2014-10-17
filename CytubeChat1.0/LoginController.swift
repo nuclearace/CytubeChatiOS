317667fb0e21aba33e10c86b436a9364bc2f14f1
Erik <nuclear.ace@gmail.com>
2014-10-16 23:16:07 -0400

    Continue work on login

---
+++ CytubeChat1.0/LoginController.swift
@@ -0,0 +1,17 @@
+//
+//  LoginController.swift
+//  CytubeChat1.0
+//
+//  Created by Erik Little on 10/16/14.
+//  Copyright (c) 2014 Tracy Cage Industries. All rights reserved.
+//
+
+import UIKit
+
+class LoginController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+}
